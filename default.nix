2a2b69d8a57b5831cc64540a00009ac1b44aa9e9
Átila Saraiva <atilasaraiva@gmail.com>
2022-04-29 15:08:59 -0300

    added install phase

--- default.nix
+++ default.nix
@@ -1,7 +1,13 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
+  pname = "torchtest";
+  version = "WIP";
   src = ./.;
 
   nativeBuildInputs = [ cmake ninja libtorch-bin ];
+
+  installPhase = ''
+    install -Dm755 dcgan $out/bin/dcgan
+  '';
 }
